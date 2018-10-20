bd3d895211152c3ebd7cc3ae587ea48a938fa9f6
Mikalai_Klepchukou <Mikalai_Klepchukou@epam.com>
2018-10-20 15:20:38 +0300

    fix: fix install dependancy

--- install.js
+++ install.js
@@ -3,6 +3,8 @@ const args = ['install'];
 const appOpts = { stdio: 'inherit', cwd: 'client', shell: true };
 require('child_process').spawn('npm', args, appOpts);
 
+const appOpts = { stdio: 'inherit', cwd: 'linkedInParser', shell: true };
+require('child_process').spawn('npm', args, appOpts);
 
 const serverOpts = { stdio: 'inherit', cwd: 'server', shell: true };
 require('child_process').spawn('npm', args, serverOpts);
