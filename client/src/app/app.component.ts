ed5f8fa1571ace2c0fc1d66c1d72aad408369743
ValiantsinT <VT@gmil.com>
2018-10-20 16:17:13 +0300

    added dynamic for game

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { SocketService } from './services/socket.service';
 enum Status {
@@ -6,6 +6,14 @@ enum Status {
     Auth
 }
 
+interface GameStepData {
+    fields: {
+        id: 1 | 2 | 3 | 4,
+        cards: any[]
+    }[];
+    myHp: number;
+    enemyHp: number;
+}
 interface SelectedCard {
     type: string;
     person: any;
@@ -16,7 +24,7 @@ interface SelectedCard {
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
     public myCard = [
         {
             firstName: 'Valiantsin1',
@@ -47,35 +55,7 @@ export class AppComponent {
         },
     ];
     public myActiveCard = [];
-    public enemyActiveCard = [
-        {
-            firstName: 'Valiantsin4',
-            lastName: 'Tsikhanau',
-            picture: 'https://static.cdn.epam.com/avatar/60655420492f5fca5a2f840c132d7e82.jpg',
-            jobPosition: '...',
-            skil1: 'Skil1',
-            skil2: 'Skil2',
-            skil3: 'Skil3',
-        },
-        {
-            firstName: 'Valiantsi5',
-            lastName: 'Tsikhanau',
-            picture: 'https://static.cdn.epam.com/avatar/60655420492f5fca5a2f840c132d7e82.jpg',
-            jobPosition: '...',
-            skil1: 'Skil1',
-            skil2: 'Skil2',
-            skil3: 'Skil3',
-        },
-        {
-            firstName: 'Valiantsin6',
-            lastName: 'Tsikhanau',
-            picture: 'https://static.cdn.epam.com/avatar/60655420492f5fca5a2f840c132d7e82.jpg',
-            jobPosition: '...',
-            skil1: 'Skil1',
-            skil2: 'Skil2',
-            skil3: 'Skil3',
-        },
-    ];
+    public enemyActiveCard = [];
     public enemyCard = [{
         firstName: 'Valiantsin1',
         lastName: 'Tsikhanau',
@@ -106,12 +86,12 @@ export class AppComponent {
     ];
     public readonly Status = Status;
     public status: Status = Status.GameField;
-    public myHp = 100;
+    public myHp = 50;
     public enemyHp = 100;
 
     private socket: SocketIOClient.Socket;
     private attackStateArray = {
-        me : null,
+        me: null,
         enemy: null
     };
     constructor(
@@ -119,7 +99,16 @@ export class AppComponent {
     ) {
         this.socket = this.socketService.getSocket();
     }
-
+    public ngOnInit() {
+        this.socket.on('onStepChange', (data: GameStepData) => {
+            this.myHp = data.myHp;
+            this.enemyHp = data.enemyHp;
+            this.enemyCard = data.fields[0].cards;
+            this.myCard = data.fields[3].cards;
+            this.enemyActiveCard = data.fields[1].cards;
+            this.myActiveCard = data.fields[2].cards;
+        });
+    }
     public drop(event: CdkDragDrop<string[]>) {
         if (event.previousContainer === event.container) {
             moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
@@ -128,12 +117,16 @@ export class AppComponent {
                 event.container.data,
                 event.previousIndex,
                 event.currentIndex);
-            this.socket.emit('onStep', [
-                { id: 1, cards: this.myCard },
-                { id: 2, cards: this.myActiveCard },
-                { id: 3, cards: this.enemyActiveCard },
-                { id: 4, cards: this.enemyCard },
-            ]);
+            this.socket.emit('onStep', {
+                fields: [
+                    { id: 1, cards: this.myCard },
+                    { id: 2, cards: this.myActiveCard },
+                    { id: 3, cards: this.enemyActiveCard },
+                    { id: 4, cards: this.enemyCard },
+                ],
+                myHp: this.myHp,
+                enemyHp: this.enemyHp
+            });
         }
     }
 
--- server/src/service/socket/socket.service.ts
+++ server/src/service/socket/socket.service.ts
@@ -2,76 +2,88 @@ import { injectable } from 'inversify';
 import SocketIO from 'socket.io';
 
 interface GameStepData {
-  id: 1 | 2 | 3 | 4;
-  cards: any[];
+    fields: [{
+        id: 1 | 2 | 3 | 4,
+        cards: any[]
+    }];
+    myHp: number;
+    enemyHp: number;
 }
 
 @injectable()
 export class SocketService {
-  private static instance: SocketService;
-  private clients: SocketIO.Socket[] = [];
+    private static instance: SocketService;
+    private clients: SocketIO.Socket[] = [];
 
-  private constructor() {
-    if (SocketService.instance) {
-      throw new Error('You try to destroy singleton');
+    private constructor() {
+        if (SocketService.instance) {
+            throw new Error('You try to destroy singleton');
+        }
     }
-  }
 
-  public static getInstance() {
-    if (!SocketService.instance) {
-      SocketService.instance = new SocketService();
+    public static getInstance() {
+        if (!SocketService.instance) {
+            SocketService.instance = new SocketService();
+        }
+        return SocketService.instance;
     }
-    return SocketService.instance;
-  }
 
-  public async setSocket(socketIO: SocketIO.Server): Promise<void | Response> {
+    public async setSocket(socketIO: SocketIO.Server): Promise<void | Response> {
 
-    socketIO.on('connection', (client: SocketIO.Socket) => {
-      this.clients.push(client);
-      if (this.clients.length === 2) {
-        client.emit('onReady');
-      }
+        socketIO.on('connection', (client: SocketIO.Socket) => {
+            this.clients.push(client);
+            if (this.clients.length === 2) {
+                client.emit('onReady');
+            }
 
-      client.on('disconnect', () => this.onDisconnect(client));
-      client.on('onStep', (data) => this.notifyOtherUser(client, data));
-    });
-  }
+            client.on('disconnect', () => this.onDisconnect(client));
+            client.on('onStep', (data) => this.notifyOtherUser(client, data));
+        });
+    }
 
-  public notifyOtherUser(client: SocketIO.Socket, data: GameStepData[]): void {
-    // const otherClient = this.clients.find((c) => c.id !== client.id);
-    // const newDate = this.swapStepData(data);
-    // otherClient.emit('onStepChange', newDate);
-  }
+    public notifyOtherUser(client: SocketIO.Socket, data: GameStepData): void {
+        const otherClient = this.clients.find((c) => c.id !== client.id);
+        console.log(data);
+        console.log(`=======================`);
+        const newDate = this.swapStepData(data);
+        console.log(newDate);
+        otherClient.emit('onStepChange', newDate);
+    }
 
-  private onDisconnect(client: SocketIO.Socket): void {
-    this.clients.splice(this.clients.indexOf(client), 1);
-  }
+    private onDisconnect(client: SocketIO.Socket): void {
+        this.clients.splice(this.clients.indexOf(client), 1);
+    }
 
-  private swapStepData(data: GameStepData[]): GameStepData[] {
-    const newData: GameStepData[] = [];
+    private swapStepData(data: GameStepData): GameStepData {
 
-    data.forEach((gameStepData: GameStepData) => {
-      if (gameStepData.id === 1) {
-        const newGameStepData: GameStepData = { id: 4, cards: gameStepData.cards };
-        newData.push(newGameStepData);
-      }
 
-      if (gameStepData.id === 4) {
-        const newGameStepData: GameStepData = { id: 1, cards: gameStepData.cards };
-        newData.push(newGameStepData);
-      }
+        data.fields.map((gameStepData: any) => {
+            if (gameStepData.id === 1) {
+                gameStepData.id = 4;
+                return gameStepData;
+            }
 
-      if (gameStepData.id === 2) {
-        const newGameStepData: GameStepData = { id: 3, cards: gameStepData.cards };
-        newData.push(newGameStepData);
-      }
+            if (gameStepData.id === 4) {
+                gameStepData.id = 1;
+                return gameStepData;
+            }
 
-      if (gameStepData.id === 3) {
-        const newGameStepData: GameStepData = { id: 2, cards: gameStepData.cards };
-        newData.push(newGameStepData);
-      }
-    });
+            if (gameStepData.id === 2) {
+                gameStepData.id = 3;
+                return gameStepData;
+            }
 
-    return newData;
-  }
+            if (gameStepData.id === 3) {
+                gameStepData.id = 2;
+                return gameStepData;
+            }
+
+        });
+
+        let tmp = data.myHp;
+        data.myHp = data.enemyHp;
+        data.enemyHp = tmp;
+
+        return data;
+    }
 }
