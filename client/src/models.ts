3eb402702ec6f57c8d1722ddf0dca01b6935053c
dmitryRomankov <strmr@tut.by>
2018-10-29 11:41:13 +0300

    added interfaces for player and cards

---
+++ client/src/models.ts
@@ -0,0 +1,25 @@
+export enum Status {
+  Init,
+  Fetching,
+  Success,
+  Error
+}
+
+export interface Player {
+  firstName: string;
+  lastName: string;
+  health: number;
+}
+
+export interface Card {
+  name: string;
+  image: string;
+  skills: string[];
+  education: number;
+  connections: number;
+  currentPosition: string;
+}
+
+export interface Deck {
+  deck: Card[];
+}
