0894b8667a2449269f0c399e11afa45011fca163
Uladzislau_Padolski <Uladzislau_Padolski@epam.com>
2018-11-01 13:03:15 +0300

    feat: added actions for cards

--- client/src/store/cards/cards.action.ts
+++ client/src/store/cards/cards.action.ts
@@ -5,7 +5,12 @@ export enum CardsActionTypes {
   GetCards = '[cards] Get Cards',
   GetCardsSuccess = '[cards] GetCards (Success)',
   GetCardsError = '[cards] GetCards (Error)',
-  MoveCardToHandFromDeck = '[cards] Move card to hand from deck',
+  GetCardsFromDeckToMe = '[cards] Move cards to me from deck',
+  GetCardsFromDeckToEnemy = '[cards] Move cards to enemy from deck',
+  MoveMyCardToBattle = '[cards] Move my card to battle',
+  MoveEnemyCardToBattle = '[cards] Move enemy card to battle',
+  DeleteMyCardFromBattle = '[cards] Delete my card from battle field',
+  DeleteEnemyCardFromBattle = '[cards] Delete enemy card from battle field'
 }
 
 export class GetCards implements Action {
@@ -15,21 +20,58 @@ export class GetCards implements Action {
 export class GetCardsSuccess implements Action {
   public readonly type = CardsActionTypes.GetCardsSuccess;
 
-  constructor(public payload: Card[]) { }
+  constructor(public payload: Card[]) {}
 }
 
 export class GetCardsError implements Action {
   public readonly type = CardsActionTypes.GetCardsError;
 
-  constructor(public payload: Error) { }
+  constructor(public payload: Error) {}
 }
 
-export class MoveCardToHandFromDeck implements Action {
-  public readonly type = CardsActionTypes.MoveCardToHandFromDeck;
+export class GetCardsFromDeckToMe implements Action {
+  public readonly type = CardsActionTypes.GetCardsFromDeckToMe;
+
+  constructor(public payload: { amount: number }) {}
+}
+
+export class GetCardsFromDeckToEnemy implements Action {
+  public readonly type = CardsActionTypes.GetCardsFromDeckToEnemy;
+
+  constructor(public payload: { amount: number }) {}
+}
+
+export class MoveMyCardToBattle implements Action {
+  public readonly type = CardsActionTypes.MoveMyCardToBattle;
+
+  constructor(public payload: { id: number }) {}
+}
+
+export class MoveEnemyCardToBattle implements Action {
+  public readonly type = CardsActionTypes.MoveEnemyCardToBattle;
+
+  constructor(public payload: { id: number }) {}
+}
+
+export class DeleteMyCardFromBattle implements Action {
+  public readonly type = CardsActionTypes.DeleteMyCardFromBattle;
+
+  constructor(public payload: { id: number }) {}
+}
+
+export class DeleteEnemyCardFromBattle implements Action {
+  public readonly type = CardsActionTypes.DeleteEnemyCardFromBattle;
+
+  constructor(public payload: { id: number }) {}
 }
 
 export type CardsActions =
   | GetCards
   | GetCardsSuccess
   | GetCardsError
-  | MoveCardToHandFromDeck;
+  | GetCardsFromDeckToMe
+  | GetCardsFromDeckToEnemy
+  | MoveMyCardToBattle
+  | MoveEnemyCardToBattle
+  | DeleteMyCardFromBattle
+  | DeleteEnemyCardFromBattle;
