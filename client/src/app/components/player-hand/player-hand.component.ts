b5595df1bcd5eb4ae033bffba660045ab1b0aa70
dmitryRomankov <strmr@tut.by>
2018-11-01 17:16:19 +0300

    component with cards on player's hand

---
+++ client/src/app/components/player-hand/player-hand.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-player-hand',
+  templateUrl: './player-hand.component.html',
+  styleUrls: ['./player-hand.component.scss']
+})
+export class PlayerHandComponent implements OnInit {
+
+  constructor() { }
+
+ public ngOnInit(): void {}
+
+}
