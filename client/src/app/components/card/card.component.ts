8a40cf85f14148016bb46c7b9d73f958aa57af5b
dmitryRomankov <strmr@tut.by>
2018-11-14 13:51:50 +0300

    card refactoring

--- client/src/app/components/card/card.component.ts
+++ client/src/app/components/card/card.component.ts
@@ -1,21 +1,27 @@
 import {
-    Component,
-    OnInit,
+  Component,
+  OnInit,
+  Input
 } from '@angular/core';
 import { faUsb } from '@fortawesome/free-brands-svg-icons';
 import { faShieldAlt } from '@fortawesome/free-solid-svg-icons';
+
+import { Card } from 'models';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
 })
+
 export class CardComponent implements OnInit {
-  constructor() {}
+  @Input() public card: Card;
+
+  constructor() { }
 
   faUsb = faUsb;
   faShieldAlt = faShieldAlt;
 
-  isFront = false;
-  ngOnInit() {}
+  isFront = true;
+  ngOnInit() { }
 }
