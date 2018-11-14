241431fdf2cef74135b2d9957d15f42f1bec4305
Anton Pankou <Anton_Pankou@epam.com>
2018-11-14 14:12:19 +0300

    Integration of Sidebar component

--- client/src/app/components/infobar/infobar.component.ts
+++ client/src/app/components/infobar/infobar.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfobarComponent implements OnInit {
 
+  public fstPlayerHP = 39;
+  public scndPlayerHP = 35;
+  public fstPlayerAvatar = 'https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/cc/cc97ee90c11a8378ee78cc8c776bb694210da1d9_full.jpg';
+  public scndPlayerAvatar = 'https://m.media-amazon.com/images/M/MV5BMzc1YTA1ZjItMWRhMy00ZTBlLTkzNTgtNTc4ZDE3YTM3ZDk2XkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_SY1000_SX1000_AL_.jpg';
+  public fstPlayercardLeft = 10;
+  public scndPlayercardLeft = 7;
+
   constructor() { }
 
-  ngOnInit() {
+  public ngOnInit(): void {
+    console.log('Component has been rendered');
+  }
+
+  public turnTrigger(): void {
+    alert('Fuck you jerk ;)');
   }
 
 }
