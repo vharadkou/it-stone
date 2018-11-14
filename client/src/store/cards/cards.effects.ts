cd4d24ee9b646aa5939823df5f7360899ef53636
dmitryRomankov <strmr@tut.by>
2018-11-14 13:56:09 +0300

    some rafactoring

--- client/src/store/cards/cards.effects.ts
+++ client/src/store/cards/cards.effects.ts
@@ -10,11 +10,13 @@ import { Card } from 'models';
 
 @Injectable()
 export class CardsEffects {
+  public baseUrl = 'http://www.mocky.io/v2/5beacbc22f00004723da3c9a';
+  public secondUrl = 'http://www.mocky.io/v2/5be983f82e00005f00f14631';
 
   @Effect() public getCards$: Observable<Action> = this.actions$.pipe(
     ofType<cardActions.GetCards>(cardActions.CardsActionTypes.GetCards),
     switchMap((action: cardActions.GetCards) =>
-      this.http.get('http://www.mocky.io/v2/5bd6f6183500000523fd7e40').pipe(
+      this.http.get(this.baseUrl).pipe(
         map((data: Card[]) => new cardActions.GetCardsSuccess(data)),
         catchError((error) => of(new cardActions.GetCardsError(error)))
       )
