2d2b07038ba8d9b8a3f379fb120948c2a7f2542e
dmitryRomankov <strmr@tut.by>
2018-11-14 13:54:23 +0300

    re-writing of player hand

--- client/src/app/components/player-hand/player-hand.component.ts
+++ client/src/app/components/player-hand/player-hand.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+
+import { State } from 'store';
+import * as CardsAction from 'store';
+import { Card } from 'models';
 
 @Component({
   selector: 'app-player-hand',
   templateUrl: './player-hand.component.html',
   styleUrls: ['./player-hand.component.scss']
 })
+
 export class PlayerHandComponent implements OnInit {
+  public allCards: Card[];
 
-  constructor() { }
+  constructor(private store: Store<State>) { }
 
- public ngOnInit(): void {}
+  public ngOnInit(): void {
+    this.store.dispatch(new CardsAction.GetCards());
 
+    this.store.select((s) => s.cards)
+      .subscribe((cards) => {
+        this.allCards = cards.cards;
+        console.log(this.allCards);
+      });
+  }
 }
