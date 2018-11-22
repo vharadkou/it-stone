fa5821855c1fffa5700e942ef7c6d15fa64a433b
Pavel <40125581+lichmanuyk@users.noreply.github.com>
2018-11-22 16:37:04 +0300

    feat: update cards init state (#12)

--- client/src/store/cards/cards.initial.ts
+++ client/src/store/cards/cards.initial.ts
@@ -3,41 +3,7 @@ import { CardsState } from './interfaces';
 
 export const initialState: CardsState = {
   status: Status.Init,
-  cards: [
-     {
-      id: 1,
-      name: 'Artsiom',
-      surname: 'Marozaua',
-      image: 'https://media.licdn.com/dms/image/C5603AQFZ0cLw7TZNKQ/profile-displayphoto-shrink_800_800/0?e=1545264000&v=beta&t=PClUjbVszSX0U7EHV-jD75tIuIg81hXKk4SsaBqjYVk',
-      skills: [
-        'SQL', '.NET', 'C#', 'React'
-      ],
-      hp: 39,
-      damage: 30,
-    },
-    {
-      id: 2,
-      name: 'Denis',
-      surname: 'Marozaua',
-      image: 'https://media.licdn.com/dms/image/C5603AQFZ0cLw7TZNKQ/profile-displayphoto-shrink_800_800/0?e=1545264000&v=beta&t=PClUjbVszSX0U7EHV-jD75tIuIg81hXKk4SsaBqjYVk',
-      skills: [
-        'SQL', '.NET', 'C#', 'React'
-      ],
-      hp: 29,
-      damage: 20,
-    },
-    {
-      id: 3,
-      name: 'Kate Marozaua',
-      surname: 'Marozaua',
-      image: 'https://media.licdn.com/dms/image/C5603AQFZ0cLw7TZNKQ/profile-displayphoto-shrink_800_800/0?e=1545264000&v=beta&t=PClUjbVszSX0U7EHV-jD75tIuIg81hXKk4SsaBqjYVk',
-      skills: [
-        'SQL', '.NET', 'C#', 'React'
-      ],
-      hp: 49,
-      damage: 40,
-    },
-  ],
+  cards: [],
   deck: [1, 2, 3],
   myCards: [],
   enemyActiveCards: [],
