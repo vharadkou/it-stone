e0faf1fa91fbc5c83f86aca272a98223464b717b
dmitryRomankov <strmr@tut.by>
2018-12-19 15:32:41 +0300

    has rewrote reducers with spread operator

--- client/src/store/cards/cards.reducer.ts
+++ client/src/store/cards/cards.reducer.ts
@@ -47,27 +47,35 @@ export const cardsReducer = (
       };
 
     case CardsActionTypes.MoveMyCardsWithinArray:
-      state.myCards.splice(action.payload.currentIndex, 0, state.myCards.splice(action.payload.previousIndex, 1)[0]);
       return {
-        ...state
+        ...state,
+        myCards: [...state.myCards.splice(
+          action.payload.currentIndex, 0, state.myCards.splice(action.payload.previousIndex, 1)[0]
+        ), ...state.myCards]
       };
 
     case CardsActionTypes.MoveEnemyCardsWithinArray:
-      state.enemyCards.splice(action.payload.currentIndex, 0, state.enemyCards.splice(action.payload.previousIndex, 1)[0]);
       return {
-        ...state
+        ...state,
+        enemyCards: [...state.enemyCards.splice(
+          action.payload.currentIndex, 0, state.enemyCards.splice(action.payload.previousIndex, 1)[0]
+        ), ...state.enemyCards]
       };
 
     case CardsActionTypes.MoveMyActiveCardsWithinArray:
-      state.myActiveCards.splice(action.payload.currentIndex, 0, state.myActiveCards.splice(action.payload.previousIndex, 1)[0]);
       return {
-        ...state
+        ...state,
+        myActiveCards: [...state.myActiveCards.splice(
+          action.payload.currentIndex, 0, state.myActiveCards.splice(action.payload.previousIndex, 1)[0]
+        ), ...state.myActiveCards]
       };
 
     case CardsActionTypes.MoveEnemyActiveCardsWithinArray:
-      state.enemyActiveCards.splice(action.payload.currentIndex, 0, state.enemyActiveCards.splice(action.payload.previousIndex, 1)[0]);
       return {
-        ...state
+        ...state,
+        enemyActiveCards: [...state.enemyActiveCards.splice(
+          action.payload.currentIndex, 0, state.enemyActiveCards.splice(action.payload.previousIndex, 1)[0]
+        ), ...state.enemyActiveCards]
       };
 
     case CardsActionTypes.GetMyBattleCard:
