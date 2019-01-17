5ff4a9dd8c58b4eb58852b5e0176e8005c24cec3
dmitryRomankov <strmr@tut.by>
2019-01-17 11:18:50 +0300

    deleted unnecessary file

--- server/src/service/socket/actions.ts
+++
@@ -1,8 +0,0 @@
-export enum CardsActionTypes {
-  MoveMyCardsWithinArray = '[card] Move My Cards Within Array',
-  MoveEnemyCardsWithinArray = '[card] Move Enemy Cards Within Array',
-  MoveMyActiveCardsWithinArray = '[card] Move My Active Cards Within Array',
-  MoveEnemyActiveCardsWithinArray = '[card] Move Enemy Active Cards Within Array',
-  GetMyBattleCard = '[cards] Move my card to battle',
-  GetEnemyBattleCard = '[cards] Move enemy card to battle',
-};
