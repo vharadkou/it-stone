c437018ff6e041fa4ce72c9d66deb04a9d5e7f9f
Uladzimir Dobryk <Uladzimir_Dobryk@epam.com>
2019-02-28 16:44:44 +0300

    add return types

--- server/src/controller/card/card.controller.ts
+++ server/src/controller/card/card.controller.ts
@@ -2,6 +2,7 @@ import { controller, httpGet, httpPost, httpDelete } from 'inversify-express-uti
 import { Request, Response } from 'express';
 import { inject } from 'inversify';
 import { CardRepository } from './../../service/card/card.repository';
+import { Card } from '../../models/new-card';
 
 @controller('/api')
 export class CardController {
@@ -10,25 +11,25 @@ export class CardController {
     ) { }
 
     @httpGet('/get-cards/')
-    public async getSkills(request: Request, response: Response){ 
+    public async getSkills(request: Request, response: Response): Promise<Card[] | Response>{ 
         try {
-            return await this.cardRepository.getCards();
+            return this.cardRepository.getCards();
           } catch (error) {
-            return response.json(error);
+            response.json(error);
         }
     }
 
     @httpPost('/save-card/')
-    public async saveCard(request: Request, response: Response){
+    public async saveCard(request: Request, response: Response):Promise<void | Response>{
         const card = request.body;
 
         try {
-            await this.cardRepository.saveCard(card.id, card.name, card.surname, card.image, card.skills, card.hp, card.damage);
+            this.cardRepository.saveCard(card.id, card.name, card.surname, card.image, card.skills, card.hp, card.damage);
   
             response.send({status: 'Successful'});            
 
         } catch (error){
-            return response.json(error);
+            response.json(error);
         }
     }
 
@@ -36,12 +37,12 @@ export class CardController {
     public async deleteCard(request: Request, response: Response): Promise<void | Response>{
         const requestId = request.body.id;
         try {
-            await this.cardRepository.deleteCard(requestId);
+            this.cardRepository.deleteCard(requestId);
 
             response.send({status: 'Successful'});            
 
           } catch (error) {
-            return response.json(error);
+            response.json(error);
         }
     } 
 }
