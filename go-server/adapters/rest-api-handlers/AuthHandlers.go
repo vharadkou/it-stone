ad922b1e60cb818d231a31277cce1a7043db5e3e
baturevich_nikita <baturevichnike@gmail.com>
2019-10-02 01:48:09 +0300

    check CI/CD

--- go-server/adapters/rest-api-handlers/AuthHandlers.go
+++ go-server/adapters/rest-api-handlers/AuthHandlers.go
@@ -44,7 +44,7 @@ func NewAuthHandler(userRepository repository.UserRepository, userSearcher searc
 
 func (h *authHandler) Login(params login.LoginParams) middleware.Responder {
 	if params.LoginForm == nil {
-		errMsg := "The request body is empty!"
+		errMsg := "The request body is empty!123123"
 		return login.NewLoginDefault(http.StatusInternalServerError).WithPayload(&models.Error{
 			Code:    http.StatusInternalServerError,
 			Message: &errMsg,
