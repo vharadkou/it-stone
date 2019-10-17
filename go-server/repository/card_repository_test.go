28a77d420b204b571dfa223bbb31dd48c06fb61b
BaturevichNikita <baturevich_nikita@outlook.com>
2019-10-17 11:14:41 +0300

    fix tests

--- go-server/repository/card_repository_test.go
+++ go-server/repository/card_repository_test.go
@@ -184,7 +184,7 @@ func TestCardRepository_DeleteCard_WrongClient(t *testing.T) {
 
 func TestCardRepository_UpdateCard_Positive(t *testing.T) {
 	t.Parallel()
-	card := domain.Card{}
+	card := make(map[string]interface{})
 	data := make(map[string]interface{})
 	firestoreClientMock := &firestoremocks.FirestoreClient{}
 	clientFuncMock := client_firestore_mock.NewFirestoreClientFuncMock(firestoreClientMock, nil)
@@ -194,7 +194,7 @@ func TestCardRepository_UpdateCard_Positive(t *testing.T) {
 
 	cardRepository := NewCardRepository(clientFuncMock)
 
-	updatedCard, err := cardRepository.UpdateCard(context.Background(), "some id", &card)
+	updatedCard, err := cardRepository.UpdateCard(context.Background(), "some id", card)
 	assert.NotNil(t, updatedCard)
 	assert.Nil(t, err)
 
@@ -203,20 +203,20 @@ func TestCardRepository_UpdateCard_Positive(t *testing.T) {
 
 func TestCardRepository_UpdateCard_WrongClient(t *testing.T) {
 	t.Parallel()
-	card := domain.Card{}
+	card := make(map[string]interface{})
 	errorMock := errors.New("some error")
 
 	clientFuncMock := client_firestore_mock.NewFirestoreClientFuncMock(nil, errorMock)
 
 	cardRepository := NewCardRepository(clientFuncMock)
-	updatedCard, err := cardRepository.UpdateCard(context.Background(), "some id", &card)
+	updatedCard, err := cardRepository.UpdateCard(context.Background(), "some id", card)
 	assert.NotNil(t, err)
 	assert.Nil(t, updatedCard)
 }
 
 func TestCardRepository_UpdateCard_ErrorUpdateOneByID(t *testing.T) {
 	t.Parallel()
-	card := domain.Card{}
+	card := make(map[string]interface{})
 	errorMock := errors.New("some error")
 
 	firestoreClientMock := &firestoremocks.FirestoreClient{}
@@ -226,7 +226,7 @@ func TestCardRepository_UpdateCard_ErrorUpdateOneByID(t *testing.T) {
 	firestoreClientMock.On("Close").Return(nil)
 
 	cardRepository := NewCardRepository(clientFuncMock)
-	updatedCard, err := cardRepository.UpdateCard(context.Background(), "some id", &card)
+	updatedCard, err := cardRepository.UpdateCard(context.Background(), "some id", card)
 	assert.Nil(t, updatedCard)
 	assert.NotNil(t, err)
 	firestoreClientMock.AssertExpectations(t)
